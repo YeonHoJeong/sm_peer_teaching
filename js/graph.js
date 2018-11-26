ee433e393e5ff4f46cbf526df1d8bb6db035e9e3
YeonHoJeong <dusgh2101@naver.com>
2018-11-26 23:54:03 +0900

    문장과 신뢰도 추가

--- js/graph.js
+++ js/graph.js
@@ -1,5 +1,7 @@
 var ConvertDataAndStart = function(resData){
     $(".wordcloud").remove(); // wordcloud 삭제요청 없으면 무시됨.
+    $(".text").remove();
+    $(".confidence").remove();
     var caption_text = resData.description.captions[0].text;
     var caption_confidence = resData.description.captions[0].confidence;
     var tags_list = resData.description.tags;
@@ -24,6 +26,13 @@ var ConvertDataAndStart = function(resData){
 };
 var graph_start = function (frequency_list, text, confidence){
 
+    /* 기존 D3에 데이터를 대입 했을 때, 이런 형식으로 집어 넣었을 것이다.
+    var frequency_list = [
+        {"text":"study","size":40},
+        {"text":"motion","size":15},
+    ];
+    이 자료에서도 똑같은 형식으로 데이터를 집어 넣는다. 다만 외부에서 JSON 객체를 받아서 그래프에 맞게 변환 시킬 뿐이다.
+    */
 
     var color = d3.scale.linear()
         .domain([0,1,2,3,4,5,6,10,15,20,100])
@@ -32,6 +41,8 @@ var graph_start = function (frequency_list, text, confidence){
     d3.layout.cloud().size([800, 300])
         .words(frequency_list)
         .rotate(function(d) { return d.rotate; })
+        .font('Impact')
+        .padding(5)
         .fontSize(function(d) { return d.size; })
         .on("end", draw)
         .start();
@@ -42,9 +53,7 @@ var graph_start = function (frequency_list, text, confidence){
             .attr("height", 350)
             .attr("class", "wordcloud")
             .append("g")
-            // without the transform, words words would get cutoff to the left and top, they would
-            // appear outside of the SVG area
-            .attr("transform", "translate(320,200)")
+            .attr("transform", "translate(300,100)")
             .selectAll("text")
             .data(words)
             .enter().append("text")
@@ -54,9 +63,10 @@ var graph_start = function (frequency_list, text, confidence){
                 return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
             })
             .text(function(d) { return d.text; });
-        d3.select("body").append("span")
-            .text(text)
-            .text(confidence);
+        d3.select("body").append("div").attr("class","text").append('span')
+            .text("인식 문장 : "+text);
+        d3.select("body").append("div").attr("class","confidence").append("span")
+            .text("문장 유사도 : " + confidence);
     }
 
 };
